73fdb09aae5adfe32e5fd92dc19154a7fb27bb17
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-09-06 17:29:45 +0530

    Create day0-mean_median_mode.py

---
+++ 10_days_of_statistics/day0-mean_median_mode.py
@@ -0,0 +1,21 @@
+import collections
+
+def mean(lst):
+    return round(sum(lst)/float(len(lst) if len(lst)!=0 else 1),1)
+
+def median(lst):
+    lst.sort()
+    lst_len=len(lst)
+    return round(lst[(lst_len+1)//2],1) if lst_len%2 != 0 else mean(lst[(lst_len-1)//2:][:2])
+  
+def mode(lst):
+    lst.sort()
+    elemnt = collections.Counter(lst).most_common()
+    first_elemnt = elemnt[0]
+    return sorted([e[0] for e in elemnt if e[1]==first_elemnt[1]])[0]
+  
+n = eval(input())
+x = list(map(int,input().split(" ")))
+print(mean(x))
+print(median(x))
+print(mode(x))

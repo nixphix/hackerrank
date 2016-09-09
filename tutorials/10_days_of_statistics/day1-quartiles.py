a40f1b6a5dc91ccbc295e30943626f91da0576af
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2016-09-09 16:46:06 +0530

    Update day1-quartiles.py

--- tutorials/10_days_of_statistics/day1-quartiles.py
+++ tutorials/10_days_of_statistics/day1-quartiles.py
@@ -6,3 +6,13 @@ def median(lst):
     return round(lst[(lst_len+1)//2],1) if lst_len%2 != 0 else mean(lst[(lst_len-1)//2:][:2])
 
 def odd_even(lst):
+    n = len(lst)//2
+    return tuple(lst[:n],lst[-n:])
+    
+n = input()
+x = map(int,input().split(" "))
+splitx = odd_even(x)
+
+print(median(splitx[0]))
+print(median(x))
+print(median(splitx[1]))
